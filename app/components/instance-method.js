76a70b8038131ffb4ae103ef0e8e199aa43bfb4c
Trek Glowacki <trek@groupon.com>
2014-12-27 09:55:19 -0600

    Show method arguemnts as comma separated list

--- app/components/instance-method.js
+++ app/components/instance-method.js
@@ -1,6 +1,10 @@
 import InstanceItemComponent from "./instance-item";
+var get = Ember.get;
 
 export default InstanceItemComponent.extend({
   itemType: 'method',
-  routeName: 'klass.methods.method'
+  routeName: 'klass.methods.method',
+  parameterSentence: function(){
+    return get(this, 'item.params').mapProperty('name').join(', ');
+  }.property('item.params.@each')
 });
