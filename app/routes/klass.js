44d59e7de677fd6617f3c9886b58b296f505b3ce
Trek Glowacki <trek@groupon.com>
2014-12-27 09:54:41 -0600

    Schedule waypoint destruction. Not sure where this best goes.

--- app/routes/klass.js
+++ app/routes/klass.js
@@ -5,7 +5,11 @@ export default Ember.Route.extend({
   model: function(params){
     return ajax('/docs/' + params.classId + '.json');
   },
-  beforeModel: function(){
-    window.scrollTo(0,0);
+  afterModel: function(){
+    // megajank. View#willDestroyElement is scheduled after
+    // route entering. Not sure where to properly schedule this.
+    setTimeout(function(){
+      window.scrollTo(0,0);
+    }, 10);
   }
 });
